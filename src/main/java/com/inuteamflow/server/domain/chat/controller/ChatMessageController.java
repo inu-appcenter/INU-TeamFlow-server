cbd4ff74b5d56e3db5489f1d3bb72a5beb29e6ea
heeyoung <heeyoung054@gmail.com>
2026-07-23 18:36:48 +0900

    🐛fix: WebSocket 메시지 전송 시 인증되지 않은 Principal 로 인한 NPE 방지

--- src/main/java/com/inuteamflow/server/domain/chat/controller/ChatMessageController.java
+++ src/main/java/com/inuteamflow/server/domain/chat/controller/ChatMessageController.java
@@ -4,6 +4,8 @@ import com.inuteamflow.server.domain.chat.dto.request.ChatMessageSendRequest;
 import com.inuteamflow.server.domain.chat.service.ChatMessageService;
 import com.inuteamflow.server.domain.user.entity.User;
 import com.inuteamflow.server.domain.user.entity.UserDetailsImpl;
+import com.inuteamflow.server.global.exception.error.CustomErrorCode;
+import com.inuteamflow.server.global.exception.error.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -31,7 +33,10 @@ public class ChatMessageController {
     }
 
     private User extractUser(Principal principal) {
-        UsernamePasswordAuthenticationToken authentication = (UsernamePasswordAuthenticationToken) principal;
+        if (!(principal instanceof UsernamePasswordAuthenticationToken authentication)) {
+            // CONNECT 인증 없이(재연결 등으로) SEND가 들어온 경우 예외 반환 (NPE 방지)
+            throw new RestApiException(CustomErrorCode.JWT_INVALID);
+        }
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         return userDetails.getUser();
     }
