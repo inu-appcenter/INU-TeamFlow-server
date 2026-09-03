f7451083a51d8b103247a846cd4eb42790e71efd
heeyoung <heeyoung054@gmail.com>
2026-09-03 20:29:14 +0900

    🐛 fix : dev/prod 배포 컨테이너에 AI_SERVER_API_KEY 누락 문제 수정

--- src/main/java/com/inuteamflow/server/domain/intip/scheduler/IntipSyncScheduler.java
+++ src/main/java/com/inuteamflow/server/domain/intip/scheduler/IntipSyncScheduler.java
@@ -12,7 +12,7 @@ public class IntipSyncScheduler {
     private final IntipSyncService intipSyncService;
 
     @Scheduled(cron = "0 */15 * * * *") // 15분마다, 서버 타임존 기준
-//    @Scheduled(cron = "0/10 * * * * *") // 테스트용: 10초마다
+    //    @Scheduled(cron = "0/10 * * * * *") // 테스트용: 10초마다
     public void run() {
         intipSyncService.sync();
     }
