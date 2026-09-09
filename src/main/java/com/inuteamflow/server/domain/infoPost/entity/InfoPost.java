f0a4f6f62364eb1d5a87ed4d702f1e5b30e1a694
손동민 <sondmmm0308@gmail.com>
2026-09-09 19:25:09 +0900

    🐛fix: Varchar(255)으로 제한되던 속성 Text로 전환

--- src/main/java/com/inuteamflow/server/domain/infoPost/entity/InfoPost.java
+++ src/main/java/com/inuteamflow/server/domain/infoPost/entity/InfoPost.java
@@ -30,7 +30,7 @@ public class InfoPost extends BaseEntity {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Column(name = "source_url")
--- src/main/java/com/inuteamflow/server/domain/team/entity/Team.java
+++ src/main/java/com/inuteamflow/server/domain/team/entity/Team.java
@@ -24,6 +24,7 @@ public class Team extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @Enumerated(EnumType.STRING)
